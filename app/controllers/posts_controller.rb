fde183cb5c55274522cae91c35807031f69e0de3
sappy920 <sae.920@gamil.com>
2020-11-02 20:45:40 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,16 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
 
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(cheked: false)
+    else
+      post.update(checked: true)
+      
+    end
 
-
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
